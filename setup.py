b975ffc5e334eb74db442628d8b6a3fc2a9a639f
S4NKALP <sankalptharu50028@gmail.com>
2025-12-12 12:22:30 +0545

    change package name to devgen-cli for pypi compatiable

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Setup script for devgen package.
 from setuptools import find_packages, setup
 
 setup(
-    name="devgen",
+    name="devgen-setup",
     version="0.2.0",
     packages=find_packages(),
     include_package_data=True,
