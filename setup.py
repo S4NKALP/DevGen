fe7da77ed670df5f351a4d7d4232c459ef79eec8
S4NKALP <sankalptharu50028@gmail.com>
2025-12-14 07:58:09 +0545

    chore(root): Bump version to 0.2.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="devgen-cli",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
