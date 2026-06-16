fca0e344220e0fb85e22b3d5e3113958abc571af
S4NKALP <sankalptharu50028@gmail.com>
2026-06-16 15:12:54 +0545

    chore(root): bump version to 0.2.9
    Updated version and dependency constraints for release.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="devgen-cli",
-    version="0.2.8",
+    version="0.2.9",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
